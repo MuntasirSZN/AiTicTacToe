7fd1b4dd6df70ef8c80ab9ffbea9b06304ddde5a
Muntasir Mahmud <mahmudurphp@gmail.com>
2024-07-09 20:52:58 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ function checkWinner(board, player) {
             return true;
         }
     }
-    //return (board[0][0] === player && board[1][1] === player && board[2][2] === player) ||
-           //(board[0][2] === player && board[1][1] === player && board[2][0] === player);
 }
 
 // Helper function to check if the board is full
@@ -32,8 +30,6 @@ function minimax(board, depth, isMaximizing) {
                 }
             }
         }
-        //console.log(`Maximizing: depth=${depth}, maxEval=${maxEval}`);
-        //return maxEval;
     } else {
         let minEval = Infinity;
         for (let i = 0; i < 3; i++) {
@@ -46,8 +42,6 @@ function minimax(board, depth, isMaximizing) {
                 }
             }
         }
-        //console.log(`Minimizing: depth=${depth}, minEval=${minEval}`);
-        //return minEval;
     }
 }
 
@@ -69,8 +63,6 @@ function bestMove(board) {
             }
         }
     }
-    //console.log(`Best move: ${move.i}, ${move.j} with value: ${bestVal}`);
-    //return move;
 }
 
 // Update the board UI
